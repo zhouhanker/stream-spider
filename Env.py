76c99e350871f136f45aab5c6c1669333d50c08a
zhouhan <zhouhanker@gmail.com>
2024-02-01 13:44:40 +0800

    Fix Env

--- Env.py
+++ Env.py
@@ -1,29 +1,19 @@
 import subprocess
 
-from tqdm import tqdm
-
 
 def generate_env_file():
-	result = subprocess.run(['pip', 'freeze'], capture_output=True, text=True, check=True)
-	with open('requirements.txt', 'w') as requirements_file, tqdm(total=len(result.stdout.splitlines()),
-																  bar_format="{l_bar}{bar:40}{r_bar}", ncols=100,
-																  desc='Generate req', ascii=True) as pbar:
-		for line in result.stdout.splitlines():
-			requirements_file.write(line + '\n')
-			pbar.update(1)
+	try:
+		result = subprocess.run(['pip', 'install', '-r', 'requirements.txt'], capture_output=True, text=True, check=True)
+		print(result.stdout)
+	except subprocess.CalledProcessError as e:
+		print(f"Error: {e}")
+		print(e.stdout)
 
 
 def install_py_lib():
 	try:
-		result = subprocess.run(
-			['pip', 'install', '-r', 'requirements.txt'],
-			stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE,
-			text=True,
-			check=True,
-			encoding='utf-8')
-		for line in tqdm(result.stdout.splitlines(), desc='Installing', ncols=100, ascii=True):
-			pass
+		result = subprocess.run(['pip', 'install', '-r', 'requirements.txt'], capture_output=True, text=True, check=True)
+		print(result.stdout)
 		print('Installation completed successfully ....')
 	except subprocess.CalledProcessError as e:
 		print(f"Error: {e}")
